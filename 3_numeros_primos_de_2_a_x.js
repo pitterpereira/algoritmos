de30e81538400a45d5b36b582d10939d0a180ab1
Pitter Pereira da Costa <pitterpereira@gmail.com>
2023-02-10 14:25:12 -0300

    Números primos

---
+++ 3_numeros_primos_de_2_a_x.js
@@ -0,0 +1,20 @@
+function numerosPrimos(numero) {
+    let primos = [];
+    
+    for(let i=2; i<=numero; i++){
+        let ePrimo = true;
+
+        // Testa se há algum divisor de 2 até o iterador atual
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                ePrimo = false; // Se houver algum, não pode ser considerado primo
+                break;
+            }
+        }
+        
+        if(ePrimo)
+            primos.push(i);
+        
+    }
+    return primos;   
+}
